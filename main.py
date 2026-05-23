c01cd6fb83deb05c15375e94ec308328219fda14
terminator <jaroslav020103@gmail.com>
2026-05-23 17:59:55 +0300

    first project commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from  dotenv import load_dotenv
+import os
+
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Вітаю в мому боті! Якщо є питання пиши /help")
+
+@dp.message(Command("about"))
+async def cmd_about(message: Message):
+    await message.answer("мене звати ярослав я вчу пайтон")
+
+@dp.message(Command("help"))
+async def cmd_help(message: Message):
+    await message.answer("команди мого бота:\n"
+                         "/start запуск бота \n"
+                         "/help список команд \n"
+                         "/about про мене \n"
+                         "/seals інфа про тюленів")
+
+@dp.message(Command("seals"))
+async def cmd_seals(message: Message):
+    await message.answer("Тюлені — це ластоногі морські ссавці, які ідеально адаптовані до холодних вод завдяки обтічній формі тіла, товстому шару жиру та здатності тривалий час перебувати під водою, виходячи на сушу або лід лише для відпочинку та розмноження.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
