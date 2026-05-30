23034ba20e1601b5f7f95a0685f3a3ddf44861a2
terminator <jaroslav020103@gmail.com>
2026-05-30 18:06:23 +0300

    добавили команду /me

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from  dotenv import load_dotenv
-import os
-
 import asyncio
+import os
+from  dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -32,6 +31,16 @@ async def cmd_help(message: Message):
 @dp.message(Command("seals"))
 async def cmd_seals(message: Message):
     await message.answer("Тюлені — це ластоногі морські ссавці, які ідеально адаптовані до холодних вод завдяки обтічній формі тіла, товстому шару жиру та здатності тривалий час перебувати під водою, виходячи на сушу або лід лише для відпочинку та розмноження.")
+
+@dp.message(Command('me'))
+async def cmd_me(message: Message):
+    user = message.from_user
+    await message.answer(
+        f"Твій профіль: \n"
+        f"Iмʼя: {user.first_name}\n"
+        f"Username: @{user.username}\n"
+        f"ID: {user.id}"
+    )
 
 async def main():
     await dp.start_polling(bot)
